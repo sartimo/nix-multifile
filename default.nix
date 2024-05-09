fd2f4d1d12ddbf45c2046febe07f4a5acfc3da67
Timo Sarkar <71646577+sartimo@users.noreply.github.com>
2024-05-09 16:30:39 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,3 +1,5 @@
+# default.nix
+
 { pkgs ? import <nixpkgs> {} }:
 
 let
@@ -8,7 +10,9 @@ let
   # Define the install phase where we output the value of the imported variable
   installPhase = ''
     mkdir -p $out
-    echo "The value of the global variable is: ${config.globalVariable}"
+    echo "The value of the global variable is: ${config.globalVariable}" > $out/message.txt
+    # Use pandoc to generate a markdown file
+    echo "# Hello, Pandoc!" | ${pkgs.pandoc}/bin/pandoc -f markdown -t html -o $out/output.html
   '';
 in
 
@@ -18,4 +22,6 @@ pkgs.stdenv.mkDerivation {
   phases = [ "installPhase" ];
   # Explicitly define the out symlink
   installPhase = installPhase;
+  # Add Pandoc as a dependency
+  buildInputs = [ pkgs.pandoc ];
 }
