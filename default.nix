477cf54d44718c54052c649f9faaf7760ad35bf1
Timo Sarkar <71646577+sartimo@users.noreply.github.com>
2024-05-09 16:26:18 +0200

    Create default.nix

---
+++ default.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  # Import the other Nix file where the global variable is defined
+  configFile = ./config.nix;
+  config = import configFile;
+
+  # Define the install phase where we output the value of the imported variable
+  installPhase = ''
+    mkdir -p $out
+    echo "The value of the global variable is: ${config.globalVariable}" > $out/message.txt
+  '';
+in
+
+pkgs.stdenv.mkDerivation {
+  name = "example";
+  src = ./.;
+  phases = [ "installPhase" ];
+  # Explicitly define the out symlink
+  installPhase = installPhase;
+}
